09682a24101db1281beef7ba43b45fc1580dc76a
Sean Murphy <sean@iamseanmurphy.com>
2025-07-30 09:20:28 +0000

    1.2.14

--- wdesignkit/build/index.asset.php
+++ wdesignkit/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-dom-ready', 'wp-edit-post', 'wp-i18n'), 'version' => '3b9ddeab05a9172593c2');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-dom-ready', 'wp-edit-post', 'wp-i18n'), 'version' => 'ec3179c085bf948644fe');
